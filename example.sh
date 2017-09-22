618dedf06bd0b904b1edc849ffeafb123cd640b1
Kensuke Numakura <knmkr3gma@gmail.com>
2017-09-22 13:03:01 -0700

    Update example.sh

--- example.sh
+++ example.sh
@@ -3,10 +3,7 @@
 TOKEN=GENOMELINKTEST
 
 REPORT_URL="https://genomicexplorer.io/v1/reports/eye-color?population=european"
-curl -H "Authorization: Bearer ${TOKEN}" ${REPORT_URL}| python -mjson.tool
+curl -H "Authorization: Bearer ${TOKEN}" ${REPORT_URL}
 
 SEQUENCE_URL="https://genomicexplorer.io/v1/genomes/sequence/?region=chr1:100000-100500"
 curl -H "Authorization: Bearer ${TOKEN}" ${SEQUENCE_URL}
-
-GENOTYPE_URL="https://genomicexplorer.io/v1/genomes/genotypes?positions=chrM:1,chrM:2,chrM:3"
-curl -H "Authorization: Bearer ${TOKEN}" ${GENOTYPE_URL}| python -mjson.tool
